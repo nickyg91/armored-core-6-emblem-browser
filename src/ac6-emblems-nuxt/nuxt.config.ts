ac935f4a936ab6966d65de5ace5bc207dad401de
Nick Ganter <nickganter91@gmail.com>
2024-01-04 11:00:09 -0600

    Added tailwind

--- src/ac6-emblems-nuxt/nuxt.config.ts
+++ src/ac6-emblems-nuxt/nuxt.config.ts
@@ -1,12 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt"],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/ui", "@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   nitro: {
     routeRules: {
-      // "/api/**": {
-      //   proxy: "https://localhost:7111/",
-      // },
       "/api/**": {
         proxy: {
           to: "https://localhost:7111/api/**",
